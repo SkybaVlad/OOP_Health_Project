5c55a52ff9ee775daa33e7f0baee0aa9aff60d77
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-04 23:01:46 +0300

    Create Medicine class

---
+++ trackers/medicine.py
@@ -0,0 +1,10 @@
+
+
+class Medicine:
+    def init(self,):
+        # стан здоровя, 0 - негативне 1 - позитивне
+        # діагноз
+        # лікування
+        # період лікування
+        # tracking actual information about height weight
+        pass
