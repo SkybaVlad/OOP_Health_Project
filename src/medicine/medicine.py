b2fc72a2d9fde6fd96aebccc07617ac1b4a97c23
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-09 01:23:26 +0300

    Add calculate_dosage method

--- src/medicine/medicine.py
+++ src/medicine/medicine.py
@@ -1,5 +1,7 @@
 class Medicine:
     def __init__(self, name, dosage, frequency):
         self.name = name
-        self.dosage = dosage  # must depends from weight and calculate authomatically
         self.frequency = frequency
+
+    def calculate_dosage(self):
+        pass
