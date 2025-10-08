6cc47d9233d1811533b1d9fcb124b1f61f094903
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-09 01:20:57 +0300

    Add getter to patient_status.py

--- src/medicine/patient_status.py
+++ src/medicine/patient_status.py
@@ -2,3 +2,6 @@ class PatientStatus:
     def __init__(self, is_sick, disease_type):
         self.is_sick = is_sick
         self.disease_type = disease_type
+
+    def is_sick(self):
+        return self.is_sick()
