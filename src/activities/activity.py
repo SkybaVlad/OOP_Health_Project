1a94eb3caf162610a1882f63559ec0a48a4c1d6b
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-09 17:50:00 +0300

    add calculate_duration method

--- src/activities/activity.py
+++ src/activities/activity.py
@@ -1,9 +1,12 @@
 class Activity:
-    def __init__(self, sport_type, place, start_time, duration):
+    def __init__(self, sport_type, place, start_time, end_time):
         self.sport_type = sport_type
         self.place = place
         self.start_time = start_time
-        self.duration = duration
+        self.end_time = end_time
 
     def burn_calories(self):
         pass
+
+    def calculate_duration(self):
+        return self.end_time - self.start_time
