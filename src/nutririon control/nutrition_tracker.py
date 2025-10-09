985774a5fcffd7cb8d19edcff3daa2db86eba2d4
Ivanko Ivan <ivanko.ivan@lll.kpi.ua>
2025-10-09 18:24:25 +0300

    delete get consumed

--- src/nutririon control/nutrition_tracker.py
+++ src/nutririon control/nutrition_tracker.py
@@ -12,8 +12,5 @@ class Nutrition:
         self.meals.append(f"{meal_name} - {calories}")
         print(f"{meal_name} - {calories} calories was added to the nutrition")
 
-    def get_consumed_calories(self):
-        return self.consumed_calories
-
     def get_remaining_calories(self):
         return self.calories_remaining
