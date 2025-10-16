8bf9bd627aada3c03f8891c3cb9bda3240e3f251
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-16 15:30:10 +0300

    Edit constructor, add do activity methos, rename burn_calories method and implement it, edit calculate_duration method

--- src/activities/activity.py
+++ src/activities/activity.py
@@ -1,12 +1,14 @@
 class Activity:
-    def __init__(self, sport_type, place, start_time, end_time):
-        self.sport_type = sport_type
-        self.place = place
-        self.start_time = start_time
-        self.end_time = end_time
+    def __init__(self, activity_name, duration):
+        self.activity_name = activity_name
+        self.duration = duration
 
-    def burn_calories(self):
+    def do_activity(self):
         pass
 
+    def count_of_burned_calories(self):
+        # calculate calories and return it, until return a const value
+        return 180
+
     def calculate_duration(self):
-        return self.end_time - self.start_time
+        return self.duration
