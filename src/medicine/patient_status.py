b796119c38256612dc51b23fcab5cdbb8f3b2936
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-16 16:51:58 +0300

    Add setter and getter for disease_type field and edit constructor

--- src/medicine/patient_status.py
+++ src/medicine/patient_status.py
@@ -1,10 +1,16 @@
 class PatientStatus:
-    def __init__(self, is_sick, disease_type):
-        self.is_sick = is_sick
-        self.disease_type = disease_type
+    def __init__(self):
+        self.is_sick = None
+        self.disease_type = None
 
     def get_is_sick_status(self):
         return self.is_sick()
 
-    def set_is_sick_status(self, value):
-        self.is_sick = value
+    def set_is_sick_status(self, is_sick_status):
+        self.is_sick = is_sick_status
+
+    def get_disease_type(self):
+        return self.disease_type
+
+    def set_disease_type(self, disease_name):
+        self.disease_type = disease_name
