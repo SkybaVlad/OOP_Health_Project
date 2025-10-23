0eb573f803c672a6115cf5766f91adcfb01bcca2
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-23 22:50:02 +0300

    Add set_weight method and deleted weight_value parameter from constructor

--- src/weight/weight.py
+++ src/weight/weight.py
@@ -1,12 +1,16 @@
 class Weight:
-    def __init__(self, value):
-        self.value = value
+    def __init__(self):
+        self.value = None
+        # add list or hashmap to storage a history of get_weight (зважувань)
 
     def get_weight(self):
         return self.value
 
-    def add_weight(self, weight):
-        self.value += weight
+    def set_weight(self, value_of_weight):
+        self.value = value_of_weight
 
-    def remove_weight(self, weight):
-        self.value -= weight
+    # def add_weight(self, weight):
+    #     self.value += weight
+    #
+    # def remove_weight(self, weight):
+    #     self.value -= weight
