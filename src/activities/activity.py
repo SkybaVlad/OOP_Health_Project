dfe7f267052616a1d7511d858c6af9ee0d75401c
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-25 00:29:06 +0300

    Add activity_name param in create_specific_activity_type_object method and implement get_activities_in_specific_date method

--- src/activities/activity.py
+++ src/activities/activity.py
@@ -6,39 +6,34 @@ class Activity:
 
     def __init__(self):
         self.specific_activity_type_object = None
-        self.manager_of_activities = ActivityContainer()
+        self.manager_of_activities = ActivityContainer() # maybe storage objects instead a activity_name
 
     def count_of_burned_calories(self):
         self.specific_activity_type_object.calculate_count_of_burned_calories()
 
-    def set_specific_activity_type_object(self, intensity, start_time, end_time):
-        self.specific_activity_type_object = SpecificActivityType(
-            intensity, start_time, end_time
-        )
-        # add new object to container class add_activity()
+    def create_specific_activity_type_object(self, activity_name, intensity, start_time, end_time, data_of_activity):
+        self.specific_activity_type_object = SpecificActivityType(activity_name, intensity, start_time, end_time)
+        self.manager_of_activities.add_activity(self.specific_activity_type_object, data_of_activity)
 
     def calculate_duration_of_activity(self):
         self.specific_activity_type_object.calculate_duration_of_activity()
 
     def get_history_of_all_activities(self):
-        pass
+        self.manager_of_activities.get_all_activities()
 
-    def get_activities_in_specific_date(self):
-        pass
+    def get_activities_in_specific_date(self, data_of_activity):
+        self.manager_of_activities.get_activity_in_specific_date(data_of_activity)
 
 
 class SpecificActivityType:
 
-    def __init__(
-        self, intensity_of_activity, start_time_of_activity, end_time_of_activity
-    ):
+    def __init__(self, activity_name, intensity_of_activity, start_time_of_activity, end_time_of_activity):
+        self.activity_name = activity_name
         self.intensity_of_activity = intensity_of_activity  # 1 to 10
         self.start_time_of_activity = start_time_of_activity
         self.end_time_of_activity = end_time_of_activity
 
-    def calculate_count_of_burned_calories(
-        self,
-    ):
+    def calculate_count_of_burned_calories(self):
         return (
             5
             * pow(self.intensity_of_activity, 1.2)
