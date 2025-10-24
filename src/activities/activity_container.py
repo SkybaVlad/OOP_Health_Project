e7e0d5357c6bdd3f4090a39b79b050a2a179b5f8
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-24 18:24:22 +0300

    Add ActivityContainer class and implement method to manage list of activities

---
+++ src/activities/activity_container.py
@@ -0,0 +1,19 @@
+class ActivityContainer:
+
+    def __init__(self):
+        dictionary = {}
+
+    def add_activity(self):
+        pass
+
+    # def remove_activity(self):
+    #     pass
+    #
+    # def remove_all_activities(self):
+    #     pass
+
+    def get_activity_in_specific_date(self):
+        pass
+
+    def get_all_activities(self):
+        pass
