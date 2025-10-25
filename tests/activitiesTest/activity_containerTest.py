525ddee2734fdbefd26c63f2e7ab252f7fd5a5b3
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-25 09:44:11 +0300

    Edit tests

--- tests/activitiesTest/activity_containerTest.py
+++ tests/activitiesTest/activity_containerTest.py
@@ -1,63 +1,68 @@
 import unittest
 from src.activities.activity_container import ActivityContainer
+from src.activities.activity import SpecificActivityType
 
 
 class ActivityContainerTest(unittest.TestCase):
     def setUp(self):
         self.activity_container_object = ActivityContainer()
+        self.activity_type_duplicate_1 = SpecificActivityType("Football", 8, 30, 20)
+        self.activity_type_duplicate_2 = SpecificActivityType("Volleyball", 3, 10, 15)
+        self.activity_type_unique_1 = SpecificActivityType("Running", 10, 50, 70)
+        self.activity_type_unique_2 = SpecificActivityType("Swimming", 3, 8, 19)
 
     def test_constructor(self):
         self.assertIsInstance(self.activity_container_object, ActivityContainer)
 
     def test_add_activity_method(self):
-        self.activity_container_object.add_activity("Football", "03.05.2007")
-        self.assertIn("03.05.2007", self.activity_container_object.dictionary.keys())
-        self.assertEqual(
-            self.activity_container_object.dictionary["03.05.2007"][0], "Football"
+        self.activity_container_object.add_activity(
+            self.activity_type_unique_1, "03.05.2007"
+        )
+        self.assertIn(
+            "03.05.2007", self.activity_container_object.dictionary.keys()
+        )  # maybe use get_all_dates_method
+        self.assertIsInstance(
+            self.activity_container_object.dictionary["03.05.2007"][0],
+            SpecificActivityType,
         )
 
     def test_add_activity_method_with_duplicate_data(self):
-        self.activity_container_object.add_activity("Football", "03.05.2007")
-        self.activity_container_object.add_activity("Volleyball", "03.05.2007")
-        self.assertIn("03.05.2007", self.activity_container_object.dictionary.keys())
-        self.assertEqual(
-            self.activity_container_object.dictionary["03.05.2007"][0], "Football"
+        self.activity_container_object.add_activity(
+            self.activity_type_duplicate_1, "03.05.2007"
+        )
+        self.activity_container_object.add_activity(
+            self.activity_type_duplicate_2, "03.05.2007"
         )
         self.assertEqual(
-            self.activity_container_object.dictionary["03.05.2007"][1], "Volleyball"
-        )
-
-    def test_add_activity_method_with_two_different_activities(self):
-        self.activity_container_object.add_activity("Football", "03.05.2007")
-        self.activity_container_object.add_activity("Volleyball", "02.05.2007")
-        self.assertIn("03.05.2007", self.activity_container_object.dictionary.keys())
-        self.assertIn("02.05.2007", self.activity_container_object.dictionary.keys())
-        self.assertEqual(
-            self.activity_container_object.dictionary["03.05.2007"][0], "Football"
+            self.activity_container_object.dictionary["03.05.2007"][0],
+            self.activity_type_duplicate_1,
         )
         self.assertEqual(
-            self.activity_container_object.dictionary["02.05.2007"][0], "Volleyball"
+            self.activity_container_object.dictionary["03.05.2007"][1],
+            self.activity_type_duplicate_2,
         )
 
     # need test with invalid data but need add typehints
 
     def test_get_activity_in_specific_date_method(self):
-        self.activity_container_object.add_activity("Football", "03.05.2007")
-        self.activity_container_object.add_activity("Volleyball", "02.05.2007")
+        self.activity_container_object.add_activity(
+            self.activity_type_unique_1, "03.05.2007"
+        )
         self.assertEqual(
             self.activity_container_object.get_activity_in_specific_date("03.05.2007")[
                 0
             ],
-            "Football",
+            self.activity_type_unique_1,
         )
 
     def test_get_activity_in_specific_date_method_with_duplicate_data(self):
-        self.activity_container_object.add_activity("Football", "03.05.2007")
-        self.activity_container_object.add_activity("Volleyball", "03.05.2007")
+        self.activity_container_object.add_activity(
+            self.activity_type_duplicate_1, "03.05.2007"
+        )
+        self.activity_container_object.add_activity(
+            self.activity_type_duplicate_2, "03.05.2007"
+        )
         self.assertEqual(
             self.activity_container_object.get_activity_in_specific_date("03.05.2007"),
-            ["Football","Volleyball"]
+            [self.activity_type_duplicate_1, self.activity_type_duplicate_2],
         )
-        self.assertIn("Football", self.activity_container_object.get_activity_in_specific_date(
-            "03.05.2007"))
-        self.assertIn("Volleyball", self.activity_container_object.get_activity_in_specific_date("03.05.2007"))
