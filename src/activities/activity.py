6f9ff74d994749829ecc43fbc6830e42a9e7d49b
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-25 12:03:12 +0300

    Add tests for Activity Class

--- src/activities/activity.py
+++ src/activities/activity.py
@@ -8,7 +8,7 @@ class Activity:
         self.specific_activity_type_object = None
         self.container_of_activities = ActivityContainer()
 
-    def count_of_burned_calories(self):
+    def count_of_burned_on_specific_activity(self):
         self.specific_activity_type_object.calculate_count_of_burned_calories()
 
     def create_specific_activity_type_object(
@@ -21,14 +21,22 @@ class Activity:
             self.specific_activity_type_object, data_of_activity
         )
 
-    def calculate_duration_of_activity(self):
-        self.specific_activity_type_object.calculate_duration_of_specific_activity()
+    # maybe add get_total_burned_calories()
+
+    # maybe add get_statistics_in_some_period(start_data,end_data)
+
+    def calculate_duration_of_specific_activity(self):
+        return (
+            self.specific_activity_type_object.calculate_duration_of_specific_activity()
+        )
 
     def get_history_of_all_activities(self):
         self.container_of_activities.get_all_activities()
 
     def get_activities_in_specific_date(self, data_of_activity):
-        self.container_of_activities.get_activity_in_specific_date(data_of_activity)
+        return self.container_of_activities.get_activity_in_specific_date(
+            data_of_activity
+        )
 
 
 class SpecificActivityType:
--- tests/activitiesTest/activitiesTest.py
+++ tests/activitiesTest/activitiesTest.py
@@ -1,77 +1,110 @@
 import unittest
 from src.activities.activity import Activity, SpecificActivityType
+from src.activities.activity_container import ActivityContainer
 
 
-class MyTestCase(unittest.TestCase):
+class TestActivityClass(unittest.TestCase):
+
+    def setUp(self):
+        self.activity = Activity()
 
     def test_constructor_of_activity_class(self):
-        activity = Activity()
         self.assertIsInstance(
-            activity, Activity, "Is not an instance of Activity Class"
+            self.activity, Activity, "Is not an instance of Activity Class"
         )
 
-    def test_field_value_of_created_object(self):
-        activity_object = Activity()
+    def test_fields_in_constructor(self):
         self.assertEqual(
-            activity_object.specific_activity_type_object, None, "Field is not None"
+            self.activity.specific_activity_type_object, None, "Is not a None"
         )
-
-    def test_field_value_when_its_created(self):
-        activity_object = Activity()
-        activity_object.set_specific_activity_type_object(10, 20, 30)
         self.assertIsInstance(
-            activity_object.specific_activity_type_object,
-            SpecificActivityType,
-            "Field is not a object of SpecificActivityType Class",
+            self.activity.container_of_activities,
+            ActivityContainer,
+            "Is not an instance of ActivityContainer Class",
         )
 
     def test_set_specific_activity_type_object_method(self):
-        activity_object = Activity()
-        activity_object.set_specific_activity_type_object(10, 20, 30)
-        self.assertEqual(
-            activity_object.specific_activity_type_object.intensity_of_activity,
-            10,
-            "Field intensity consists not expected value",
+        self.activity.create_specific_activity_type_object(
+            "Football", 5, 30, 80, "03.05.2007"
         )
-        self.assertEqual(
-            activity_object.specific_activity_type_object.start_time_of_activity,
-            20,
-            "Field intensity consists not expected value",
-        )
-        self.assertEqual(
-            activity_object.specific_activity_type_object.end_time_of_activity,
-            30,
-            "Field intensity consists not expected value",
+        self.assertIsInstance(
+            self.activity.specific_activity_type_object,
+            SpecificActivityType,
+            "Is not an instance of SpecificActivityType Class",
         )
 
-    def test_constructor_of_specific_activity_type_class(self):
-        specific_activity_type_object = SpecificActivityType(10, 20, 30)
-        self.assertEqual(
-            specific_activity_type_object.intensity_of_activity,
-            10,
-            "Intensity of Activity Class",
+    def test_calculate_duration_of_specific_activity_method(self):
+        self.activity.create_specific_activity_type_object(
+            "Football", 5, 30, 80, "03.05.2007"
         )
         self.assertEqual(
-            specific_activity_type_object.start_time_of_activity,
-            20,
-            "Start Time of Activity Class",
+            self.activity.calculate_duration_of_specific_activity(),
+            50,
+            "Duration calculated not true",
+        )
+
+    def test_get_activities_in_specific_date_method(self):
+        self.activity.create_specific_activity_type_object(
+            "Football", 5, 30, 80, "03.05.2007"
         )
         self.assertEqual(
-            specific_activity_type_object.end_time_of_activity,
-            30,
-            "End Time of Activity Class",
+            type(self.activity.get_activities_in_specific_date("03.05.2007")),
+            type([]),
+            "list is not returned",
+        )
+
+    def test_get_activities_in_specific_data_with_duplication_data(self):
+        self.activity.create_specific_activity_type_object(
+            "Football", 5, 30, 80, "03.05.2007"
+        )
+        self.activity.create_specific_activity_type_object(
+            "Volleyball", 5, 30, 80, "03.05.2007"
+        )
+        self.assertEqual(
+            len(
+                self.activity.container_of_activities.get_activity_in_specific_date(
+                    "03.05.2007"
+                )
+            ),
+            2,
+        )
+
+
+class TestSpecificActivityTypeClass(unittest.TestCase):
+    def setUp(self):
+        self.specific_activity_type = SpecificActivityType("Football", 7, 40, 70)
+
+    def test_constructor(self):
+        self.assertIsInstance(self.specific_activity_type, SpecificActivityType)
+
+    def test_getter_get_name(self):
+        self.assertEqual(
+            self.specific_activity_type.get_name_of_specific_activity(), "Football"
+        )
+
+    def test_getter_get_intensity(self):
+        self.assertEqual(
+            self.specific_activity_type.get_intensity_of_specific_activity(), 7
+        )
+
+    def test_getter_get_start_time(self):
+        self.assertEqual(
+            self.specific_activity_type.get_start_time_of_specific_activity(), 40
+        )
+
+    def test_getter_get_end_time(self):
+        self.assertEqual(
+            self.specific_activity_type.get_end_time_of_specific_activity(), 70
         )
 
     def test_calculate_duration_of_activity_method(self):
-        specific_activity_type_object = SpecificActivityType(10, 20, 30)
         self.assertEqual(
-            specific_activity_type_object.calculate_duration_of_activity(), 10
+            self.specific_activity_type.calculate_duration_of_specific_activity(), 30
         )
 
     def test_calculate_count_of_burned_calories_method(self):
-        specific_activity_type_object = SpecificActivityType(10, 20, 30)
         self.assertGreater(
-            specific_activity_type_object.calculate_count_of_burned_calories(), 0
+            self.specific_activity_type.calculate_count_of_burned_calories(), 0
         )
 
 
