ac938f498b1c15ae21ecec895c6fc5b8244f087d
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-27 20:53:46 +0200

    Create ActivityType module for SpecificActivityClass

--- src/activities/activity.py
+++ src/activities/activity.py
@@ -1,5 +1,5 @@
-import math
 from src.activities.activity_container import ActivityContainer
+from src.activities.activity_type import SpecificActivityType
 
 
 class Activity:
@@ -37,43 +37,3 @@ class Activity:
         return self.container_of_activities.get_activity_in_specific_date(
             data_of_activity
         )
-
-
-class SpecificActivityType:
-
-    def __init__(
-        self,
-        activity_name,
-        intensity_of_activity,
-        start_time_of_activity,
-        end_time_of_activity,
-    ):
-        self.activity_name = activity_name
-        self.intensity_of_activity = intensity_of_activity  # 1 to 10
-        self.start_time_of_activity = start_time_of_activity
-        self.end_time_of_activity = end_time_of_activity
-
-    def calculate_count_of_burned_calories(self):
-        return (
-            5
-            * pow(self.intensity_of_activity, 1.2)
-            * math.sqrt(self.calculate_duration_of_specific_activity())
-            + 0.3
-            * self.intensity_of_activity
-            * self.calculate_duration_of_specific_activity()
-        )
-
-    def calculate_duration_of_specific_activity(self):
-        return abs(self.start_time_of_activity - self.end_time_of_activity)
-
-    def get_intensity_of_specific_activity(self):
-        return self.intensity_of_activity
-
-    def get_name_of_specific_activity(self):
-        return self.activity_name
-
-    def get_start_time_of_specific_activity(self):
-        return self.start_time_of_activity
-
-    def get_end_time_of_specific_activity(self):
-        return self.end_time_of_activity
