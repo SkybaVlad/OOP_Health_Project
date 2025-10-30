93c6a27ae4b2fee95d8de69ea862f2f8105f6ca4
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-31 01:44:10 +0200

    Implement BodyMetrics class

--- src/body_metrics/body_metrics.py
+++ src/body_metrics/body_metrics.py
@@ -5,21 +5,23 @@ class BodyMetrics:
     def __init__(self):
         pass
 
-    def calculate_body_mass_index_metrics(self, user_weight, user_height):
+    def calculate_body_mass_index_metrics(self, user_weight, user_height) -> float:
         return user_weight / math.pow(user_height / 100, 2)
 
     def calculate_basal_metabolic_rate_metrics(
         self, user_weight, user_height, user_age, user_sex
-    ):
+    ) -> float:
         if user_sex == 'male':
             return 10 * user_weight + 6.25 * user_height - 5 * user_age + 5
         else:
             return 10 * user_weight + 6.25 * user_height - 5 * user_age - 161
 
-    def calculate_lean_body_mass_metrics(self, user_weight, user_percentage_of_fat):
+    def calculate_lean_body_mass_metrics(
+        self, user_weight, user_percentage_of_fat
+    ) -> float:
         return user_weight * (1 - user_percentage_of_fat / 100)
 
-    def calculate_fat_mass_metrics(self, user_weight, user_percentage_of_fat):
+    def calculate_fat_mass_metrics(self, user_weight, user_percentage_of_fat) -> float:
         lbm_metrics = self.calculate_lean_body_mass_metrics(
             user_weight, user_percentage_of_fat
         )
