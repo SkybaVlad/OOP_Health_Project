a590dda073bb43c7822802bc897d713d7b082df1
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-30 19:46:18 +0200

    Deleted parameters fron constructor

--- src/user/user_body_info.py
+++ src/user/user_body_info.py
@@ -1,11 +1,13 @@
+# from src. import UserBodyGoals
+
+
 class UserBodyInfo:
-    def __init__(self, weight: float, height: float):
-        self.weight: float = weight
-        self.height: float = height
+    def __init__(self):
+        self.weight: float = 0.0
+        self.height: float = 0.0
         self.fat_percentage: float = 0.0
-        self.body_mass_index: float = 0.0
         self.percentage_of_water_level: float = 0.0
-        self.lean_body_mass_index: float = 0
+        self.user_body_goals = None
 
     def get_weight(self):
         return self.weight
@@ -16,14 +18,8 @@ class UserBodyInfo:
     def get_fat_percentage(self):
         return self.fat_percentage
 
-    def get_body_mass_index(self):
-        return self.body_mass_index
-
     def get_percentage_of_water_level(self):
         return self.percentage_of_water_level
-
-    def get_lean_body_mass_index(self):
-        return self.lean_body_mass_index
 
     def set_weight(self, weight):
         self.weight = weight
@@ -34,11 +30,5 @@ class UserBodyInfo:
     def set_fat_percentage(self, fat_percentage):
         self.fat_percentage = fat_percentage
 
-    def set_body_mass_index(self, body_mass_index):
-        self.body_mass_index = body_mass_index
-
     def set_get_percentage_of_water_level(self, percentage_of_water_level):
         self.percentage_of_water_level = percentage_of_water_level
-
-    def set_get_lean_body_mass_index(self, lean_body_mass_index):
-        self.lean_body_mass_index = lean_body_mass_index
