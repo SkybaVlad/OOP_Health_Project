51843c1b58565c5ab17291642eca486eec69757b
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-31 23:35:17 +0200

    Adding Medication Class and MedicationReminder class

--- tests/medicineTest/patient_statusTest.py
+++
@@ -1,9 +0,0 @@
-import unittest
-from src.medicine.patient_status import PatientStatus
-
-
-class TestMedicine(unittest.TestCase):
-    def test_init_method_with_valid_params(self):
-        patient_status_obj = PatientStatus(True, 'Covid-19')
-        self.assertEqual(patient_status_obj.is_sick, True)
-        self.assertEqual(patient_status_obj.disease_type, 'Covid-19')
