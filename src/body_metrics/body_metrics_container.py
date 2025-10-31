30abc664abeea06bcf07ac1f5807a02cca9a171a
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-31 11:35:27 +0200

    Adding check statements to all methods

--- src/body_metrics/body_metrics_container.py
+++ src/body_metrics/body_metrics_container.py
@@ -13,13 +13,26 @@ class BodyMetricsContainer:
         self.dictionary = {}
 
     def add_body_mass_index_metrics(self, value, date):
-        self.dictionary[(date, BodyMetricsType.body_mass_index_metrics.value)] = value
+        if (date,BodyMetricsType.body_mass_index_metrics.value) in self.dictionary.keys():
+            self.dictionary[(date, BodyMetricsType.body_mass_index_metrics.value)].append(value)
+        else:
+            self.dictionary[(date, BodyMetricsType.body_mass_index_metrics.value)] = [value]
 
     def add_basal_metabolic_rate(self, value, date):
-        self.dictionary[(date, BodyMetricsType.basal_metabolic_rate.value)] = value
+        if (date, BodyMetricsType.basal_metabolic_rate.value) in self.dictionary.keys():
+            self.dictionary[(date, BodyMetricsType.basal_metabolic_rate.value)].append(value)
+        else:
+            self.dictionary[(date, BodyMetricsType.basal_metabolic_rate.value)] = [value]
 
     def add_lean_body_mass(self, value, date):
-        self.dictionary[(date, BodyMetricsType.lean_body_mass.value)] = value
+        if (date, BodyMetricsType.lean_body_mass.value) in self.dictionary.keys():
+            self.dictionary[(date, BodyMetricsType.lean_body_mass.value)].append(value)
+        else:
+            self.dictionary[(date, BodyMetricsType.lean_body_mass.value)] = [value]
 
     def add_fat_mass(self, value, date):
-        self.dictionary[(date, BodyMetricsType.fat_mass.value)] = value
+        if (date, BodyMetricsType.fat_mass.value) in self.dictionary.keys():
+            self.dictionary[(date, BodyMetricsType.fat_mass.value)].append(value)
+        else:
+            self.dictionary[(date, BodyMetricsType.fat_mass.value)] = [value]
+
