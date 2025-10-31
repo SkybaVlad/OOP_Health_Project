7ab88a57db456973d38265703d1980fdbde18860
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-31 21:24:07 +0200

    Adding new fields, setters and getters

--- src/user/user_body_info.py
+++ src/user/user_body_info.py
@@ -7,6 +7,10 @@ class UserBodyInfo:
         self.height: float = 0.0
         self.fat_percentage: float = 0.0
         self.percentage_of_water_level: float = 0.0
+        self.body_mass_index: float = 0.0
+        self.basal_metabolic_rate: float = 0.0
+        self.lean_body_mass: float = 0.0
+        self.fat_mass: float = 0.0
         self.user_body_goals = UserBodyGoals()
 
     def get_weight(self) -> float:
@@ -20,6 +24,18 @@ class UserBodyInfo:
 
     def get_percentage_of_water_level(self) -> float:
         return self.percentage_of_water_level
+
+    def get_body_mass_index(self) -> float:
+        return self.body_mass_index
+
+    def get_basal_metabolic_rate(self) -> float:
+        return self.basal_metabolic_rate
+
+    def get_lean_body_mass(self) -> float:
+        return self.lean_body_mass
+
+    def get_fat_mass(self):
+        return self.fat_mass
 
     def set_weight(self, weight):
         try:
@@ -49,6 +65,18 @@ class UserBodyInfo:
             print(Error)
             return
         self.percentage_of_water_level = percentage_of_water_level
+
+    def set_body_mass_index(self, body_mass_index):
+        self.body_mass_index = body_mass_index
+
+    def set_basal_metabolic_rate(self, basal_metabolic_rate):
+        self.basal_metabolic_rate = basal_metabolic_rate
+
+    def set_lean_body_mass(self, lean_body_mass):
+        self.lean_body_mass = lean_body_mass
+
+    def set_fat_mass(self, fat_mass):
+        self.fat_mass = fat_mass
 
     def _validate_data_for_setters(self, data):
         if data < 0 or data is None:
