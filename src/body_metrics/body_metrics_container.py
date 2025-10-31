2c13f489f171491ce0d3267dd26555a54217c67a
vladskyba <skyba.vlad@lll.kpi.ua>
2025-10-31 23:23:32 +0200

    Adding new add methods

--- src/body_metrics/body_metrics_container.py
+++ src/body_metrics/body_metrics_container.py
@@ -6,6 +6,10 @@ class BodyMetricsType(enum.Enum):
     basal_metabolic_rate = 'bmr'
     lean_body_mass = 'lbm'
     fat_mass = 'fm'
+    weight = 'weight'
+    height = 'height'
+    fat_percentage = 'fp'
+    percentage_of_water_level = 'fwl'
 
 
 class BodyMetricsContainer:
@@ -13,26 +17,54 @@ class BodyMetricsContainer:
         self.dictionary = {}
 
     def add_body_mass_index_metrics(self, value, date):
-        if (date,BodyMetricsType.body_mass_index_metrics.value) in self.dictionary.keys():
+        if self._in_dict(date, BodyMetricsType.body_mass_index_metrics):
             self.dictionary[(date, BodyMetricsType.body_mass_index_metrics.value)].append(value)
         else:
             self.dictionary[(date, BodyMetricsType.body_mass_index_metrics.value)] = [value]
 
     def add_basal_metabolic_rate(self, value, date):
-        if (date, BodyMetricsType.basal_metabolic_rate.value) in self.dictionary.keys():
+        if self._in_dict(date, BodyMetricsType.basal_metabolic_rate.value):
             self.dictionary[(date, BodyMetricsType.basal_metabolic_rate.value)].append(value)
         else:
             self.dictionary[(date, BodyMetricsType.basal_metabolic_rate.value)] = [value]
 
     def add_lean_body_mass(self, value, date):
-        if (date, BodyMetricsType.lean_body_mass.value) in self.dictionary.keys():
+        if self._in_dict(date, BodyMetricsType.lean_body_mass):
             self.dictionary[(date, BodyMetricsType.lean_body_mass.value)].append(value)
         else:
             self.dictionary[(date, BodyMetricsType.lean_body_mass.value)] = [value]
 
     def add_fat_mass(self, value, date):
-        if (date, BodyMetricsType.fat_mass.value) in self.dictionary.keys():
+        if self._in_dict(date, BodyMetricsType.fat_mass.value):
             self.dictionary[(date, BodyMetricsType.fat_mass.value)].append(value)
         else:
             self.dictionary[(date, BodyMetricsType.fat_mass.value)] = [value]
 
+    def add_weight(self, value, date):
+        if self._in_dict(date, BodyMetricsType.weight.value):
+            self.dictionary[(date, BodyMetricsType.weight.value)].append(value)
+        else:
+            self.dictionary[(date, BodyMetricsType.weight.value)] = [value]
+
+    def add_height(self, value, date):
+        if self._in_dict(date, BodyMetricsType.height.value):
+            self.dictionary[(date, BodyMetricsType.height.value)].append(value)
+        else:
+            self.dictionary[(date, BodyMetricsType.height.value)] = [value]
+
+    def add_fat_percentage(self, value, date):
+        if self._in_dict(date, BodyMetricsType.fat_percentage.value):
+            self.dictionary[(date, BodyMetricsType.fat_percentage.value)].append(value)
+        else:
+            self.dictionary[(date, BodyMetricsType.fat_percentage.value)] = [value]
+
+    def add_percentage_of_water_level(self, value, date):
+        if self._in_dict(date, BodyMetricsType.percentage_of_water_level.value):
+            self.dictionary[(date, BodyMetricsType.percentage_of_water_level.value)].append(value)
+        else:
+            self.dictionary[(date, BodyMetricsType.percentage_of_water_level.value)] = [value]
+
+    def _in_dict(self, date, metrics_type):
+        if (date, metrics_type) in self.dictionary.keys():
+            return True
+        return False
