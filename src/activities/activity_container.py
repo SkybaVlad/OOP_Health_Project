67d0c9ee8cdeeeacc7a042f3adf49cf22da7cff8
vladskyba <skyba.vlad@lll.kpi.ua>
2025-11-06 15:49:31 +0200

    Edit get_all_function

--- src/activities/activity_container.py
+++ src/activities/activity_container.py
@@ -9,22 +9,26 @@ class ActivityContainer:
     # need check on invalid param
 
     def add_activity(
-        self, specific_activity_object: SpecificActivityType, data_of_activity: str
+            self, specific_activity_object: SpecificActivityType, data_of_activity: str
     ):
         if data_of_activity in self.dictionary.keys():
             self.dictionary[data_of_activity].append(specific_activity_object)
         else:
             self.dictionary[data_of_activity] = [specific_activity_object]
 
-    def clear_all_activities(self):
-        pass
-
     def get_activity_in_specific_date(self, data_of_activity: str) -> list:
         return self.dictionary[data_of_activity]
 
-    def get_all_activities(self):
-        pass
-        # need sort hashtable
+    def get_all_activities(self) -> list:
+        sorted_list_of_activities = sorted(self.dictionary.items())
+        return sorted_list_of_activities
 
     def get_all_dates(self):
         pass
+
+
+activity_object1 = SpecificActivityType("Football", 400, '17:20', '19:20')
+activity_object2 = SpecificActivityType("Running", 500, '16:20', '17:20')
+object = ActivityContainer()
+object.add_activity(activity_object1, '03.05.2007')
+object.add_activity(activity_object2, '01.05.2007')
