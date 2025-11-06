f06bd3a4c79206368fabcaca9326c768a7c8e29e
vladskyba <skyba.vlad@lll.kpi.ua>
2025-11-06 14:17:59 +0200

    adding set_age method to User class and doing fields private

--- src/user/user_info.py
+++ src/user/user_info.py
@@ -1,18 +1,21 @@
 class User:
     def __init__(self, name, surname, age, sex):
-        self.name = name
-        self.surname = surname
-        self.age = age
-        self.sex = sex
+        self.__name = name
+        self.__surname = surname
+        self.__age = age
+        self.__sex = sex
 
     def get_name(self):
-        return self.name
+        return self.__name
 
     def get_surname(self):
-        return self.surname
+        return self.__surname
 
     def get_age(self):
-        return self.age
+        return self.__age
+
+    def set_age(self, age):
+        self.__age = age
 
     def get_sex(self):
-        return self.sex
+        return self.__sex
