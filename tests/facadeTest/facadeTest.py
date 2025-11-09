561ef7440712a6cab6ec0a9ceb630556c4aaab93
vladskyba <skyba.vlad@lll.kpi.ua>
2025-11-09 02:51:39 +0200

    Testing facade class

--- tests/facadeTest/facadeTest.py
+++ tests/facadeTest/facadeTest.py
@@ -1,6 +1,38 @@
 import unittest
+from buisness.facade import Facade
+from unittest.mock import patch
+from services.user.user_info import User
 
 
-class TestFacade(unittest.TestCase):
-    def test_constructor(self):
-        pass
+user = User("Vlad", 'Skyba', 18, 'mail')
+
+
+class TestFacadeSetters(unittest.TestCase):
+    def setUp(self):
+        self.facade = Facade(user)
+
+    def test_set_weight_method_with_valid_data(self):
+        self.facade.set_weight(100)
+        self.assertEqual(
+            self.facade.get_weight(), 100, "Weight value have unexpected value"
+        )
+
+    def test_set_weight_method_with_invalid_data(self):
+        self.facade.set_weight(100)
+        self.facade.set_weight(-100)
+        self.assertEqual(
+            self.facade.get_weight(), 100, "Weight value must have a previous value"
+        )
+        self.facade.set_weight(50)
+        self.facade.set_weight(None)
+        self.assertEqual(
+            self.facade.get_weight(), 50, "Weight value must have a previous value"
+        )
+
+
+class TestFacadeGetters(unittest.TestCase):
+    pass
+
+
+if '__name__' == '__main__':
+    unittest.main()
