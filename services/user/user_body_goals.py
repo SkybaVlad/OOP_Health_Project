3bd049c997ecb3281c513c43d848841622dac7d0
Ivanko Ivan <ivanko.ivan@lll.kpi.ua>
2025-11-20 21:22:30 +0200

    renewed user_body_goals.py

--- services/user/user_body_goals.py
+++ services/user/user_body_goals.py
@@ -1,22 +1,32 @@
 from services.user.user_body_info import UserBodyInfo
 from services.user.user_info import User
+from services.body_metrics.body_metrics import BodyMetrics
 
 
 class UserBodyGoals:
-    def __init__(self, user_body_info: UserBodyInfo, user: User):
+    def __init__(
+        self, user_body_info: UserBodyInfo, user: User, user_body_metrics: BodyMetrics
+    ):
         self.user_body_info = user_body_info
         self.user = user
+        self.user_body_metrics = user_body_metrics
+
         self.total_water_goal = 0
         self.total_calories_goal = 0
 
-    def set_total_water_goal(self, weight):
-        self.total_water_goal = self.calculate_total_water_goal(weight)
+    # ==========WATER===========
+    def set_total_water_goal(self):
+        weight = self.user_body_info.get_weight()
+        self.total_water_goal = weight * 30.0
 
-    def calculate_total_water_goal(self, weight):
-        return weight * 30.0
+    def get_total_water_goal(self):
+        return self.total_water_goal
+
+    # ==========CALORIES===========
+    # Set ActivityLevel
 
     def calculate_total_calories_goal(self):
-        self.body_metrics.calculate_basal_metabolic_rate_metrics = (
+        bmr = self.body_metrics.calculate_basal_metabolic_rate_metrics = (
             self.body_metrics.calculate_basal_metabolic_rate_metrics(
                 self.user_body_info.get_weight(),
                 self.user_body_info.get_height(),
@@ -24,9 +34,7 @@ class UserBodyGoals:
                 self.user.get_sex(),
             )
         )
-
-    def get_total_water_goal(self):
-        return self.total_water_goal
+        # self.total_calories_goal= bmr*activity_level придумати як рівень активності дізнаватися
 
     def get_total_calories_goal(self):
         return self.total_calories_goal
