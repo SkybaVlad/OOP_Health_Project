7d7368b6d4bfb63dfe5f25c2a7c9028b383708b0
Ivanko Ivan <ivanko.ivan@lll.kpi.ua>
2025-11-22 00:08:20 +0200

    changed goals

--- buisness/nutrition_manager.py
+++
@@ -1,9 +0,0 @@
-from services.nutrition.nutrition_tracking import NutritionType
-
-
-class NutritionManager:
-    def __init__(self):
-        self.history = []
-
-    def add_nutrition(self, new_meal: NutritionType):
-        self.history.append(new_meal)
--- services/user/user_body_goals.py
+++ services/user/user_body_goals.py
@@ -1,39 +1,18 @@
-from services.user.user_body_info import UserBodyInfo
-from services.user.user_info import User
-from services.body_metrics.body_metrics import BodyMetrics
-
-
 class UserBodyGoals:
-    def __init__(
-        self, user_body_info: UserBodyInfo, user: User, user_body_metrics: BodyMetrics
-    ):
-        self.user_body_info = user_body_info
-        self.user = user
-        self.user_body_metrics = user_body_metrics
-
-        self.total_water_goal = 0
-        self.total_calories_goal = 0
+    def __init__(self):
+        self.water_goal = 0
+        self.calories_goal = 0
 
     # ==========WATER===========
-    def set_total_water_goal(self):
-        weight = self.user_body_info.get_weight()
-        self.total_water_goal = weight * 30.0
+    def set_water_goal(self,user_water_goal: float):
+        self.water_goal = user_water_goal
 
-    def get_total_water_goal(self):
-        return self.total_water_goal
+    def get_water_goal(self):
+        return self.water_goal
 
     # ==========CALORIES===========
-    # Set ActivityLevel
+    def set_calories_goal(self,calories_goal: float):
+        self.calories_goal = calories_goal
 
-    def calculate_total_calories_goal(self):
-        bmr = self.body_metrics.calculate_basal_metabolic_rate_metrics(
-            self.user_body_info.get_weight(),
-            self.user_body_info.get_height(),
-            self.user.get_age(),
-            self.user.get_sex(),
-        )
-        activity_level = 1.2  # придумати як рівень активності дізнаватися
-        return bmr * activity_level
-
-    def get_total_calories_goal(self):
-        return self.total_calories_goal
+    def get_calories_goal(self):
+        return self.calories_goal
