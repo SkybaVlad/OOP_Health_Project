c436aa870fa7da5219b7f63022ea9b1001e56aaf
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-01 00:44:39 +0200

    Delete some methods

--- data/activity_container.py
+++ data/activity_container.py
@@ -3,25 +3,18 @@ from services.activities.activity_type import SpecificActivityType
 
 class ActivityContainer:
 
-    def __init__(self):
+    def __init__(self) -> None:
         self.dictionary = {}
 
     # need check on invalid param
 
     def add_activity(
         self, specific_activity_object: SpecificActivityType, data_of_activity: str
-    ):
+    ) -> None:
         if data_of_activity in self.dictionary.keys():
             self.dictionary[data_of_activity].append(specific_activity_object)
         else:
             self.dictionary[data_of_activity] = [specific_activity_object]
 
-    def get_activity_in_specific_date(self, data_of_activity: str) -> list:
-        return self.dictionary[data_of_activity]
-
-    def get_all_activities(self) -> list:
-        sorted_list_of_activities = sorted(self.dictionary.items())
-        return sorted_list_of_activities
-
-    def get_all_dates(self):
-        pass
+    def get_all_activities(self) -> dict:
+        return self.dictionary
