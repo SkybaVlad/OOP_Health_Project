c8ae9ebdad3efdc4e72a2b292739f2809a195f3e
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-05 22:29:15 +0200

    updates methods that working with body_metrics filtration

--- facade_logic/facade_container.py
+++ facade_logic/facade_container.py
@@ -5,9 +5,10 @@ from services.activities.activity_type import SpecificActivityType
 from data.filtration import (
     FiltrationActivity,
     ActivityCriteria,
-    FiltrationCriteria,
+    BodyMetricsCriteria,
     FiltrationBodyMetrics,
 )
+from services.nutrition.meal import Meal
 
 
 class FacadeContainer:
@@ -16,6 +17,7 @@ class FacadeContainer:
         self.activity_container = ActivityContainer()
         self.filtration_activity = FiltrationActivity()
         self.filtration_body_metrics = FiltrationBodyMetrics()
+        self.meal_container = MealContainer()
 
     def add_activity(
         self, activity_object: SpecificActivityType, data_of_activity: str
@@ -40,12 +42,13 @@ class FacadeContainer:
         self, filtration_criteria: ActivityCriteria
     ) -> list:
         self.filtration_activity.set_filtration_criteria(filtration_criteria)
-        pass
+        return self.filtration_activity.filter_by_greater_value_of_burned_calories()
 
     def filter_activity_by_lesser_value_on_calories(
         self, filtration_criteria: ActivityCriteria
     ) -> list:
-        pass
+        self.filtration_activity.set_filtration_criteria(filtration_criteria)
+        return self.filtration_activity.filter_by_less_value_of_burned_calories()
 
     def filter_activity_by_duration(
         self, filtration_criteria: ActivityCriteria
@@ -54,3 +57,26 @@ class FacadeContainer:
 
     def add_body_metrics(self, metrics_type, value, data) -> None:
         self.body_metrics_container.add_body_metrics(metrics_type, value, data)
+
+    def filter_body_metrics_by_type(self, body_metrics_criteria: BodyMetricsCriteria) -> list:
+        self.filtration_body_metrics.set_filtration_criteria(body_metrics_criteria)
+        return self.filtration_body_metrics.filter_by_type()
+
+    def filter_body_metrics_by_specific_data(self, body_metrics_criteria: BodyMetricsCriteria) -> list:
+        self.filtration_body_metrics.set_filtration_criteria(body_metrics_criteria)
+        return self.filtration_body_metrics.filter_by_specific_data()
+
+    def filter_body_metrics_period(self, body_metrics_criteria: BodyMetricsCriteria) -> list:
+        self.filtration_body_metrics.set_filtration_criteria(body_metrics_criteria)
+        return self.filtration_body_metrics.filter_by_period()
+
+    def filter_body_metrics_by_greater_value_of_burned_calories(self, body_metrics_criteria: BodyMetricsCriteria) -> list:
+        self.filtration_body_metrics.set_filtration_criteria(body_metrics_criteria)
+        return self.filtration_body_metrics.filter_by_greater_value()
+
+    def filter_body_metrics_by_lesser_value_on_calories(self, body_metrics_criteria: BodyMetricsCriteria) -> list:
+        self.filtration_body_metrics.set_filtration_criteria(body_metrics_criteria)
+        return self.filtration_body_metrics.filter_by_less_value()
+
+    def add_meal(self, meal: Meal, data):
+        self.meal_container.add_meal(meal,data)
