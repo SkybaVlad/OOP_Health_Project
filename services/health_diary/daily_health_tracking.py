17f8488fe3f48ddd7e87e76f0b714c23e8b73da5
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-06 13:43:15 +0200

    Edit constructor params in HealthDailyAnalyzer

--- services/health_diary/daily_health_tracking.py
+++ services/health_diary/daily_health_tracking.py
@@ -1,8 +1,11 @@
-from services.body_metrics.body_metrics import StrategyBodyMetricsInterface
+from services.body_metrics.body_metrics import BodyMetricsCalculator
+from services.user.user_body_info import UserBodyInfo
 from services.activities.activity_type import SpecificActivityType
 from services.nutrition.meal import Meal
 import time
 from services.time_logic import time_in_period
+from services.user.user_info import User
+from services.user.user_body_goals import UserBodyDailyGoals
 
 
 class HealthDaily:
@@ -12,7 +15,7 @@ class HealthDaily:
         self.consumed_calories_for_day: float = 0.0
         self.list_of_activities_for_day: list = []
         self.list_of_meals_for_day: list = []
-        self.drank_water: float = 0.0
+        self.drunk_water: float = 0.0
         self.sleep_duration: float = 0.0
 
     def add_burned_calories(self, calories) -> None:
@@ -22,7 +25,13 @@ class HealthDaily:
         self.list_of_activities_for_day.append(activity_object)
         self.burned_calories_for_day += activity_object.get_burned_calories()
 
-    # add method add burned calories to allow user enter burned calories (maybe)
+    # add method add_burned_calories, add_consumed_calories to allow user enter burned calories (maybe)
+
+    def add_drunk(self, amount_of_drunk_water) -> None:
+        self.drunk_water += amount_of_drunk_water
+
+    def add_sleep(self, amount_of_sleep) -> None:
+        self.sleep_duration += amount_of_sleep
 
     def add_meals(self, meal: Meal) -> None:
         self.list_of_meals_for_day.append(meal)
@@ -33,52 +42,57 @@ class HealthDailyAnalyzer:
     """This class responsible for analyze daily health. This class analyze a health_diary object and
     user_body_daily_goals and provide methods that return results of analysis"""
 
-    def __init__(self, health_diary, user_body_daily_goals):
-        self.health_diary = health_diary
+    def __init__(self, health_daily: HealthDaily, user_body_daily_goals: UserBodyDailyGoals, user_body_info: UserBodyInfo, user_info: User):
+        self.health_daily = health_daily
         self.user_body_daily_goals = user_body_daily_goals
+        self.body_metrics_calculator = BodyMetricsCalculator(user_body_info, user_info)
 
     def get_total_time_spent_on_activities_in_minutes(self) -> float:
         total_time_spent = 0.0
-        for activity in self.health_diary.list_of_activities_for_day:
+        for activity in self.health_daily.list_of_activities_for_day:
             total_time_spent += activity.get_total_time_spent_on_activities_in_minutes()
         return total_time_spent
 
     def get_remaining_of_consumed_calories(self) -> float:
         return (
             self.user_body_daily_goals.get_consumed_calories_goal()
-            - self.health_diary.consumed_calories_for_day
+            - self.health_daily.consumed_calories_for_day
         )
 
     def get_remaining_of_burned_calories(self) -> float:
         return (
             self.user_body_daily_goals.get_burned_calories_goal()
-            - self.health_diary.burned_calories_for_day
+            - self.health_daily.burned_calories_for_day
         )
 
     def get_remaining_water(self) -> float:
         return (
-            self.user_body_daily_goals.get_water_goal() - self.health_diary.drank_water
+            self.user_body_daily_goals.get_water_goal() - self.health_daily.drunk_water
         )
 
     def get_consumed_calories(self) -> float:
-        return self.health_diary.consumed_calories_for_day
+        return self.health_daily.consumed_calories_for_day
 
     def get_burned_calories(self) -> float:
-        return self.health_diary.burned_calories_for_day
+        return self.health_daily.burned_calories_for_day
 
     def get_consumed_water(self) -> float:
-        return self.health_diary.drank_water
+        return self.health_daily.drunk_water
 
     def get_body_metrics_values(self):
         pass
 
     def get_sleep_duration(self) -> float:
-        return self.health_diary.sleep_duration
+        return self.health_daily.sleep_duration
+
+    def calculate_body_mass_index(self):
 
 
-class HealthMonthAnalyzer:
-    """This class responsible for analyze a month statistics. This class analyze list of health_diary objects.
-    Constructor accepts a list of health_diary objects and start_data, end_data. Start_data and end_data have next the str format YYYY-MM-DD
+
+class HealthAnalyzerInSomePeriod:
+    """This class responsible for analyze a statistics on some period of time. This class analyze list of health_diary objects.
+    Constructor accepts a list of health_diary objects and start_data, end_data. Start_data and end_data have next
+    the str format YYYY-MM-DD
     """
 
     def __init__(self, list_of_health_diary: list, start_data: str, end_data: str):
