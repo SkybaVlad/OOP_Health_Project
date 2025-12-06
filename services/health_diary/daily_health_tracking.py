6bd80f8a6a3ed4d16228e43ac1804b7f93529c19
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-06 14:42:07 +0200

    Add new methods that analyze some metrics like total_steps, total_time_spent_on_activities (any category of activities)

--- services/health_diary/daily_health_tracking.py
+++ services/health_diary/daily_health_tracking.py
@@ -6,6 +6,7 @@ import time
 from services.time_logic import time_in_period
 from services.user.user_info import User
 from services.user.user_body_goals import UserBodyDailyGoals
+from services.activities.activity_type import ActivityCategory
 
 
 class HealthDaily:
@@ -17,15 +18,15 @@ class HealthDaily:
         self.list_of_meals_for_day: list = []
         self.drunk_water: float = 0.0
         self.sleep_duration: float = 0.0
-
-    def add_burned_calories(self, calories) -> None:
-        self.burned_calories_for_day += calories
+        self.count_of_steps_for_day = 0.0
 
     def add_activity(self, activity_object: SpecificActivityType) -> None:
         self.list_of_activities_for_day.append(activity_object)
         self.burned_calories_for_day += activity_object.get_burned_calories()
 
-    # add method add_burned_calories, add_consumed_calories to allow user enter burned calories (maybe)
+    def add_meals(self, meal: Meal) -> None:
+        self.list_of_meals_for_day.append(meal)
+        self.consumed_calories_for_day += meal.calories
 
     def add_drunk(self, amount_of_drunk_water) -> None:
         self.drunk_water += amount_of_drunk_water
@@ -33,9 +34,8 @@ class HealthDaily:
     def add_sleep(self, amount_of_sleep) -> None:
         self.sleep_duration += amount_of_sleep
 
-    def add_meals(self, meal: Meal) -> None:
-        self.list_of_meals_for_day.append(meal)
-        self.consumed_calories_for_day += meal.calories
+    def add_count_of_steps(self, count_of_steps) -> None:
+        self.count_of_steps_for_day += count_of_steps
 
 
 class HealthDailyAnalyzer:
@@ -85,11 +85,11 @@ class HealthDailyAnalyzer:
     def get_consumed_water(self) -> float:
         return self.health_daily.drunk_water
 
-    def get_body_metrics_values(self):
-        pass
-
     def get_sleep_duration(self) -> float:
         return self.health_daily.sleep_duration
+
+    def get_count_of_steps_for_day(self) -> float:
+        return self.health_daily.count_of_steps_for_day
 
     def calculate_body_mass_index(self):
         return self.body_metrics_calculator.calculate_body_mass_index_metrics()
@@ -135,3 +135,51 @@ class HealthAnalyzerInSomePeriod:
         for day in self.list_of_health_diary:
             total_burned_calories += day.burned_calories_for_day
         return total_burned_calories
+
+    def get_total_steps(self):
+        total_steps = 0.0
+        for day in self.list_of_health_diary:
+            total_steps += day.count_of_steps_for_day
+        return total_steps
+
+    def get_total_time_spent_on_cardio_activities(self):
+        total_time_spent = 0.0
+        for day in self.list_of_health_diary:
+            for activity in day.list_of_activities_for_day:
+                if activity.activity_category == ActivityCategory.Cardio.value:
+                    total_time_spent += (
+                        activity.calculate_activity_duration_in_minutes()
+                    )
+        return total_time_spent
+
+    def get_total_time_spent_on_strength_activities(self):
+        total_time_spent = 0.0
+        for day in self.list_of_health_diary:
+            for activity in day.list_of_activities_for_day:
+                if activity.activity_category == ActivityCategory.Strength.value:
+                    total_time_spent += (
+                        activity.calculate_activity_duration_in_minutes()
+                    )
+        return total_time_spent
+
+    def get_total_time_spent_on_sport_activities(self):
+        total_time_spent = 0.0
+        for day in self.list_of_health_diary:
+            for activity in day.list_of_activities_for_day:
+                if activity.activity_category == ActivityCategory.Sport.value:
+                    total_time_spent += (
+                        activity.calculate_activity_duration_in_minutes()
+                    )
+        return total_time_spent
+
+    def get_day_with_max_consumed_calories(self):
+        pass
+
+    def get_day_with_max_burned_calories(self):
+        pass
+
+    def get_day_with_max_steps(self):
+        pass
+
+    def get_day_with_max_time_spent_on_activities(self):
+        pass
