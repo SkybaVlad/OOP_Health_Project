ab533de57edc9f58b1f2f9de0e45fa8f7ddad0bf
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-06 13:42:19 +0200

    Delete strategy pattern and implement class BodyMetricsCalculator that provide methods for different calculation

--- services/body_metrics/body_metrics.py
+++ services/body_metrics/body_metrics.py
@@ -1,6 +1,7 @@
 import math
-from abc import ABC, abstractmethod
+from services.user.user_info import User
 from enum import Enum
+from services.user.user_body_info import UserBodyInfo
 
 
 class BodyMetricsType(Enum):
@@ -14,69 +15,37 @@ class BodyMetricsType(Enum):
     percentage_of_water_level = 'fwl'
 
 
-class StrategyBodyMetricsInterface(ABC):
-    @abstractmethod
-    def calculate(self):
-        pass
+class BodyMetricsCalculator:
+    def __init__(self, user_body_info: UserBodyInfo, user_info: User):
+        self.user_body_info = user_body_info
+        self.user_info = user_info
 
+    def calculate_body_mass_index_metrics(self):
+        return self.user_body_info.get_height() / math.pow(
+            self.user_body_info.get_height() / 100, 2
+        )
 
-class Context:
-    def __init__(self):
-        self.strategy = None
-
-    def set_strategy(self, strategy: StrategyBodyMetricsInterface):
-        self.strategy = strategy
-
-    def calculate(self):
-        self.strategy.calculate()
-
-
-class StrategyBMICalculator(StrategyBodyMetricsInterface):
-    def __init__(self, user_weight, user_height):
-        self.user_weight = user_weight
-        self.user_height = user_height
-
-    def calculate(self):
-        return self.user_weight / math.pow(self.user_height / 100, 2)
-
-
-class StrategyBMRCalculator(StrategyBodyMetricsInterface):
-    def __init__(self, user_weight, user_height, user_age, user_sex):
-        self.user_weight = user_weight
-        self.user_height = user_height
-        self.user_age = user_age
-        self.user_sex = user_sex
-
-    def calculate(self):
-        if self.user_sex == 'male':
+    def calculate_basal_metabolic_rate(self):
+        if self.user_info.get_sex() == 'male':
             return (
-                10 * self.user_weight + 6.25 * self.user_height - 5 * self.user_age + 5
+                10 * self.user_body_info.get_weight()
+                + 6.25 * self.user_body_info.get_height()
+                - 5 * self.user_info.get_age()
+                + 5
             )
         else:
             return (
-                10 * self.user_weight
-                + 6.25 * self.user_height
-                - 5 * self.user_age
+                10 * self.user_body_info.get_weight()
+                + 6.25 * self.user_body_info.get_height()
+                - 5 * self.user_info.get_age()
                 - 161
             )
 
-
-class StrategyLeanBodyMassCalculator(StrategyBodyMetricsInterface):
-    def __init__(self, user_weight, user_percentage_of_fat):
-        self.user_weight = user_weight
-        self.user_percentage_of_fat = user_percentage_of_fat
-
-    def calculate(self):
-        return self.user_weight * (1 - self.user_percentage_of_fat / 100)
-
-
-class StrategyFatMassCalculator(StrategyBodyMetricsInterface):
-    def __init__(self, user_weight, user_percentage_of_fat):
-        self.user_weight = user_weight
-        self.user_percentage_of_fat = user_percentage_of_fat
-
-    def calculate(self):
-        lean_body_mass_value = StrategyLeanBodyMassCalculator(
-            self.user_weight, self.user_percentage_of_fat
+    def calculate_lean_body_mass(self):
+        return self.user_body_info.get_weight() * (
+            1 - self.user_body_info.get_fat_percentage() / 100
         )
-        return self.user_weight - lean_body_mass_value
+
+    def calculate_fat_mass(self):
+        lean_body_mass_value = self.calculate_lean_body_mass()
+        return self.user_body_info.get_weight() - lean_body_mass_value
