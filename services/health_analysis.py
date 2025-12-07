c89bc485ff75a4e9ad42739b3185e492e04c9bd4
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-07 19:18:04 +0200

    Add setters for HealthDailyAnalyzer class

--- services/health_analysis.py
+++ services/health_analysis.py
@@ -6,27 +6,31 @@ from services.body_metrics.body_metrics_calculator import (
     calculate_basal_metabolic_rate,
 )
 from services.health_daily.daily_health import HealthDaily
-from services.time_logic import time_in_period
 from services.user.user_body_goals import UserBodyDailyGoals
 from services.user.user_body_info import UserBodyInfo
 from services.user.user_info import User
-from data.health_diary import HealthDiary
 
 
 class HealthDailyAnalyzer:
     """This class responsible for analyze daily health. This class analyze a health_daily object and
     user_body_daily_goals and provide methods that return results of analysis"""
 
-    def __init__(
-        self,
-        health_daily: HealthDaily,
-        user_body_daily_goals: UserBodyDailyGoals,
-        user_body_info: UserBodyInfo,
-        user_info: User,
-    ):
+    def __init__(self):
+        self.health_daily: HealthDaily | None = None
+        self.user_body_daily_goals: UserBodyDailyGoals | None = None
+        self.user_body_info: UserBodyInfo | None = None
+        self.user: User | None = None
+
+    def set_day_that_need_to_analyze(self, health_daily: HealthDaily):
         self.health_daily = health_daily
+
+    def set_user_body_daily_goals(self, user_body_daily_goals: UserBodyDailyGoals):
         self.user_body_daily_goals = user_body_daily_goals
+
+    def set_user_body_info(self, user_body_info: UserBodyInfo):
         self.user_body_info = user_body_info
+
+    def set_user_info(self, user_info: User):
         self.user = user_info
 
     def get_total_time_spent_on_activities_in_minutes(self) -> float:
@@ -147,73 +151,80 @@ class HealthDailyAnalyzer:
         return calculate_fat_mass(self.health_daily.weight, lean_body_mass_value)
 
 
-class HealthAnalyzerInSomePeriod:
+class HealthInSomePeriodAnalyzer:
     """This class responsible for analyze a statistics on some period of time. This class analyze list of health_daily objects.
     Constructor accepts a list of health_daily objects and start_data, end_data. Start_data and end_data have next
     the str format YYYY-MM-DD
     """
 
-    def __init__(self, health_diary: HealthDiary, start_data: str, end_data: str):
-        self.list_health_diary = health_diary.get_history_of_days()
-        self.start_data = start_data
-        self.end_data = end_data
+    def __init__(self):
+        self.list_health_diary: list[HealthDaily] | None = None
+        self.start_data: str | None = None
+        self.end_data: str | None = None
 
-    def get_total_time_spent_on_activities_in_minutes(self) -> float:
+    def set_list_of_days(self, list_of_health_diary: list[HealthDaily]):
+        self.list_health_diary = list_of_health_diary
+
+    def set_period_of_time(self, start_time_of_period, end_time_of_period):
+        self.start_data = start_time_of_period
+        self.end_data = end_time_of_period
+
+    def get_total_time_spent_on_activities_in_minutes_for_all_time(self) -> float:
         total_time_spent = 0.0
         for day in self.list_health_diary:
             for activity in day.list_of_activities_for_day:
                 total_time_spent += activity.calculate_activity_duration_in_minutes()
         return total_time_spent
 
-    def get_total_consumed_calories(self) -> float:
+    def get_total_consumed_calories_for_all_time(self) -> float:
         total_consumed_calories = 0.0
         for day in self.list_health_diary:
             total_consumed_calories += day.consumed_calories_for_day
         return total_consumed_calories
 
-    def get_total_burned_calories(self) -> float:
+    def get_total_burned_calories_for_all_time(self) -> float:
         total_burned_calories = 0.0
         for day in self.list_health_diary:
             total_burned_calories += day.burned_calories_for_day
         return total_burned_calories
 
-    def get_total_steps(self):
+    def get_total_steps_for_all_time(self):
         total_steps = 0.0
         for day in self.list_health_diary:
             total_steps += day.count_of_steps_for_day
         return total_steps
 
-    def get_total_time_spent_on_cardio_activities(self):
+    def get_total_time_spent_on_cardio_activities_for_all_time(self):
         total_time_spent = 0.0
         for day in self.list_health_diary:
             for activity in day.list_of_activities_for_day:
-                if activity.activity_category == ActivityCategory.Cardio.value:
+                if activity.get_activity_category() == ActivityCategory.Cardio.value:
                     total_time_spent += (
                         activity.calculate_activity_duration_in_minutes()
                     )
         return total_time_spent
 
-    def get_total_time_spent_on_strength_activities(self):
+    def get_total_time_spent_on_strength_activities_for_all_time(self):
         total_time_spent = 0.0
         for day in self.list_health_diary:
             for activity in day.list_of_activities_for_day:
-                if activity.activity_category == ActivityCategory.Strength.value:
+                if activity.get_activity_category() == ActivityCategory.Strength.value:
                     total_time_spent += (
                         activity.calculate_activity_duration_in_minutes()
                     )
         return total_time_spent
 
-    def get_total_time_spent_on_sport_activities(self):
+    def get_total_time_spent_on_sport_activities_for_all_time(self):
         total_time_spent = 0.0
         for day in self.list_health_diary:
             for activity in day.list_of_activities_for_day:
-                if activity.activity_category == ActivityCategory.Sport.value:
+                if activity.get_activity_category() == ActivityCategory.Sport.value:
                     total_time_spent += (
                         activity.calculate_activity_duration_in_minutes()
                     )
         return total_time_spent
 
-    def get_day_with_max_consumed_calories(self) -> HealthDaily | float:
+    def get_day_with_max_consumed_calories_for_all_time(self) -> HealthDaily | float:
         if len(self.list_health_diary) == 0:
             return 0.0
         day_with_max_consumed_calories = self.list_health_diary[0]
@@ -222,7 +233,7 @@ class HealthAnalyzerInSomePeriod:
                 day_with_max_consumed_calories = day.consumed_calories_for_day
         return day_with_max_consumed_calories
 
-    def get_day_with_max_burned_calories(self) -> HealthDaily | float:
+    def get_day_with_max_burned_calories_for_all_time(self) -> HealthDaily | float:
         if len(self.list_health_diary) == 0:
             return 0.0
         day_with_max_burned_calories = self.list_health_diary[0]
@@ -231,7 +242,7 @@ class HealthAnalyzerInSomePeriod:
                 day_with_max_burned_calories = day.burned_calories_for_day
         return day_with_max_burned_calories
 
-    def get_day_with_max_steps(self) -> HealthDaily | float:
+    def get_day_with_max_steps_for_all_time(self) -> HealthDaily | float:
         if len(self.list_health_diary) == 0:
             return 0.0
         day_with_max_steps = self.list_health_diary[0]
@@ -240,7 +251,9 @@ class HealthAnalyzerInSomePeriod:
                 day_with_max_steps = day.count_of_steps_for_day
         return day_with_max_steps
 
-    def get_day_with_max_time_spent_on_activities(self) -> HealthDaily | float:
+    def get_day_with_max_time_spent_on_activities_for_all_time(
+        self,
+    ) -> HealthDaily | float:
         if len(self.list_health_diary) == 0:
             return 0.0
         day_with_max_time_spent_on_activities = self.list_health_diary[0]
@@ -262,7 +275,9 @@ class HealthAnalyzerInSomePeriod:
                 day_with_max_time_spent_on_activities = day
         return day_with_max_time_spent_on_activities
 
-    def get_day_with_max_amount_of_drunk_water(self) -> HealthDaily | float:
+    def get_day_with_max_amount_of_drunk_water_for_all_time(
+        self,
+    ) -> HealthDaily | float:
         if len(self.list_health_diary) == 0:
             return 0.0
         day_with_max_amount_of_drunk_water = self.list_health_diary[0]
@@ -271,7 +286,7 @@ class HealthAnalyzerInSomePeriod:
                 day_with_max_amount_of_drunk_water = day
         return day_with_max_amount_of_drunk_water
 
-    def get_day_with_max_hours_spent_on_sleep(self):
+    def get_day_with_max_hours_spent_on_sleep_for_all_time(self):
         if len(self.list_health_diary) == 0:
             return 0.0
         day_with_max_hours_of_sleep = self.list_health_diary[0]
