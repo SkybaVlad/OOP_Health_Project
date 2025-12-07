734a3d8270dd89e383e8c774bfb5b51fa928f39b
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-07 19:17:27 +0200

    Start to implement facade_analysis

--- facade_logic/facade_analysis.py
+++ facade_logic/facade_analysis.py
@@ -1,26 +1,30 @@
-from services.health_daily.health_analysis import (
-    HealthDailyAnalyzer,
-    HealthAnalyzerInSomePeriod,
-)
+from services.health_analysis import HealthDailyAnalyzer, HealthInSomePeriodAnalyzer
+from services.health_daily.daily_health import HealthDaily
+from services.specification_for_filter import Specification
 
 
 class AnalysisFacade:
     def __init__(self):
-        self.health_daily_analyzer = HealthDailyAnalyzer()
-        self.health_analyzer = HealthAnalyzerInSomePeriod()
+        self.health_daily_analyzer: HealthDailyAnalyzer = HealthDailyAnalyzer()
+        self.health_analyzer_some_period: HealthInSomePeriodAnalyzer = (
+            HealthInSomePeriodAnalyzer()
+        )
+
+    def set_list_of_days_that_need_analyze(self, list_of_all_days: list[HealthDaily]):
+        self.health_analyzer_some_period.set_list_of_days(list_of_all_days)
+
+    def set_period_of_time_that_need_analyze(
+        self, start_period_of_time: str, end_period_of_time: str
+    ):
+        self.health_analyzer_some_period.set_period_of_time(
+            start_period_of_time, end_period_of_time
+        )
+
+    def get_result_of_analyze_some_period(self):
+        pass
 
     def get_daily_results(self):
-        health_analyzer = HealthDailyAnalyzer(
-            self.health_diary, self.user_body_daily_goals
-        )
-        return (
-            health_analyzer.get_burned_calories(),
-            health_analyzer.get_remaining_of_burned_calories(),
-            health_analyzer.get_remaining_of_consumed_calories(),
-            # body_metrics
-            health_analyzer.get_consumed_calories(),
-            health_analyzer.get_consumed_water(),
-            health_analyzer.get_remaining_water(),
-            health_analyzer.get_sleep_duration(),
-            health_analyzer.get_total_time_spent_on_activities_in_minutes(),
-        )
+        pass
+
+    def filter(self, specification: Specification):
+        pass
