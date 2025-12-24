e2d9bf19316c136c0b37bc6b9e012db621b01972
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-24 19:13:40 +0200

    add singeltone pattern to User class

--- services/user/user_info.py
+++ services/user/user_info.py
@@ -1,9 +1,19 @@
 class User:
+
+    __user_instance = None
+
+    def __new__(cls, *args, **kwargs):
+        if cls.__user_instance is None:
+            cls.__user_instance = super().__new__(cls)
+        return cls.__user_instance
+
     def __init__(self, name, surname, age, sex):
-        self.__name = name
-        self.__surname = surname
-        self.__age = age
-        self.__sex = sex
+        if not hasattr(self, 'initialized'):
+            self.__name = name
+            self.__surname = surname
+            self.__age = age
+            self.__sex = sex
+            self.initialized = True
 
     def get_name(self):
         return self.__name
@@ -19,3 +29,14 @@ class User:
 
     def get_sex(self):
         return self.__sex
+
+    def __repr__(self):
+        return f"User(name={self.__name}, surname={self.__surname}, age={self.__age}, sex={self.__sex})"
+
+
+user = User("Vlad", "Skyba", 19, "male")
+user1 = User("Maks", "Gan", 18, "female")
+print(id(user))
+print(id(user1))
+print(user)
+print(user1)
