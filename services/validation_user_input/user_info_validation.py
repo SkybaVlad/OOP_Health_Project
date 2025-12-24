8922b5de520169ea2d827f5ce8d25e6807bc6838
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-24 22:06:15 +0200

    Implement function for UserInfo class. This functions validates the inputs data

--- services/validation_user_input/user_info_validation.py
+++ services/validation_user_input/user_info_validation.py
@@ -6,24 +6,36 @@ def validate_user_name(name: str) -> bool:
         raise TypeError('Name must be a string type')
     if len(name) < 3 or len(name) > 15:
         raise ValueError('Name must be between 3 and 15 characters')
-    if name[0] >= 'a' or name[0] <= 'z':
+    if name[0] >= 'a' and name[0] <= 'z':
         raise ValueError('Name must start with upper case letter')
-    for char in name:
+    for index, char in enumerate(name):
         if not char.isalpha():
             raise ValueError('Name must contain only letters')
+        if index >= 1:
+            if name[index] < 'a':
+                raise ValueError(
+                    "All letters in word must be lower case besides first letter"
+                )
     return True
 
 
 def validate_surname(surname: str):
+    """This function validates the users surnames.
+    Like a name the surname should match all requires as a name"""
     if not isinstance(surname, str):
         raise TypeError('Surname must be a string type')
     if len(surname) < 3 or len(surname) > 15:
         raise ValueError('Surname must be between 3 and 15 characters')
-    if surname[0] >= 'a' or surname[0] <= 'z':
+    if surname[0] >= 'a' and surname[0] <= 'z':
         raise ValueError('Surname must start with upper case letter')
-    for char in surname:
+    for index, char in enumerate(surname):
         if not char.isalpha():
             raise ValueError('Surname must contain only letters')
+        if index >= 1:
+            if surname[index] < 'a':
+                raise ValueError(
+                    "All letters in word must be lower case besides first letter"
+                )
     return True
 
 
@@ -38,6 +50,6 @@ def validate_age(age: int):
 def validate_sex(user_sex: str):
     if not isinstance(user_sex, str):
         raise TypeError("User_sex value must be a string type")
-    if user_sex != 'Male' or 'Female':
+    if user_sex != 'Male' and user_sex != 'Female':
         raise ValueError("User_sex value must be 'Male' or 'Female'")
     return True
