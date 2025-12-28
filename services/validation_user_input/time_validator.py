c1cea00fc591a9be911b52ae619fee1331933ded
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-28 19:09:05 +0200

    Simplify complex time_validator_format_yyyy_mm_dd function and add functions to validate number of year, month , day

--- services/validation_user_input/time_validator.py
+++ services/validation_user_input/time_validator.py
@@ -1,12 +1,54 @@
 import datetime
+from datetime import date
 
 """This module responsible for validation data for time input"""
 
 
+def is_year_value_in_limits(year: str) -> bool:
+    """This function checks if the year value is between current years value - 1 and current years value
+    For current_year = 2027 so example check_time_limits_for_years_value(year=2027) -> true and
+    example check_time_limits_for_years_value(year=2026) -> true but if year=2025 or 2028 functions should return false
+    limits is [curr_year - 1 , curr_year]"""
+    current_year_minus_one = str(int(datetime.datetime.now().strftime("%Y")) - 1)
+    if year < current_year_minus_one or year > datetime.datetime.now().strftime("%Y"):
+        return False
+    return True
+
+
+def is_day_number_in_limits_for_month_where_count_of_days_31(
+    month_number: str, number_of_day: str
+) -> bool:
+    """This function checks if the number of day between 1 and 31 for specific month"""
+    if (
+        month_number == '01'
+        or month_number == '03'
+        or month_number == '05'
+        or month_number == '07'
+        or month_number == '08'
+        or month_number == '10'
+        or month_number == '12'
+    ):
+        if number_of_day > '31' or number_of_day < '01':
+            return False
+    return True
+
+
+def is_day_number_in_limits_for_month_where_count_of_days_30(
+    month_number: str, number_of_day: str
+) -> bool:
+    if (
+        month_number == '04'
+        or month_number == '06'
+        or month_number == '09'
+        or month_number == '11'
+    ):
+        if number_of_day < '01' or number_of_day > '30':
+            return False
+    return True
+
+
 def time_validator_format_yyyy_mm_dd(time: str):
     """Time format must have the following format: YYYY-MM-DD"""
-    if time is None:
-        raise TypeError('Date cannot be None')
     if type(time) != str:
         raise TypeError('Time must be a string')
     if len(time) != 10:
@@ -24,38 +66,28 @@ def time_validator_format_yyyy_mm_dd(time: str):
         i += 1
 
     list_of_values = time.split('-')
-    current_year_minus_one = int(datetime.datetime.now().strftime("%Y"))
-    current_year_minus_one = current_year_minus_one - 1
-    current_year_minus_one = str(current_year_minus_one)
-    if list_of_values[0] < current_year_minus_one or list_of_values[
-        0
-    ] > datetime.datetime.now().strftime("%Y"):
+    if not is_year_value_in_limits(list_of_values[0]):
         raise ValueError(
-            "Years value should be between current_year - 1 and current_year"
+            f"Years value should be between {str(int(datetime.datetime.now().strftime("%Y")) - 1)} and {datetime.datetime.now().strftime("%Y")}"
         )
-    if list_of_values[1] < '01' or list_of_values[1] > '12':
-        raise ValueError('Month value should be between 1 and 12')
-    if (
-        list_of_values[1] == '01'
-        or list_of_values[1] == '03'
-        or list_of_values[1] == '05'
-        or list_of_values[1] == '07'
-        or list_of_values[1] == '08'
-        or list_of_values[1] == '10'
-        or list_of_values[1] == '12'
-    ):
-        if list_of_values[2] > '31' or list_of_values[2] < '01':
-            raise ValueError("Day must be between 1 and 31")
 
-    if (
-        list_of_values[1] == '04'
-        or list_of_values[1] == '06'
-        or list_of_values[1] == '09'
-        or list_of_values[1] == '11'
+    if list_of_values[1] < '01' or list_of_values[1] > '12':
+        raise ValueError('Month number should be between 1 and 12')
+
+    # condition for month with max day count is 31
+    if not is_day_number_in_limits_for_month_where_count_of_days_31(
+        list_of_values[1], list_of_values[2]
+    ):
+        raise ValueError("Day must be between 1 and 31")
+
+    # condition for month with max day count is 30
+    if not is_day_number_in_limits_for_month_where_count_of_days_30(
+        list_of_values[1], list_of_values[2]
     ):
         if list_of_values[2] < '01' or list_of_values[2] > '30':
             raise ValueError("Day must be between 1 and 30")
 
+    # condition for month with max day count is 28
     if list_of_values[1] == '02':
         if list_of_values[2] < '01' or list_of_values[2] > '28':
             raise ValueError("Day must be between 1 and 28")
@@ -63,8 +95,6 @@ def time_validator_format_yyyy_mm_dd(time: str):
 
 def time_validator_format_hh_mm(time: str):
     """Time format must have the following format: HH:MM"""
-    if time is None:
-        raise TypeError('Time cannot be None')
     if type(time) != str:
         raise TypeError('Time must be a string')
     if len(time) != 5:
@@ -73,7 +103,7 @@ def time_validator_format_hh_mm(time: str):
     while i < len(time):
         if i == 2:
             if time[i] != ':':
-                raise ValueError('Between integers must be line, HH:MM')
+                raise ValueError('Between integers must be two dots like HH:MM')
         else:
             if not time[i].isdigit():
                 raise ValueError('Years must be integers')
