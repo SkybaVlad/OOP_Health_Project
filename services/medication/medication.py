6d1005f91e81f3cc581c1d8da3d9bb9d81f8f17e
vladskyba <skyba.vlad@lll.kpi.ua>
2025-12-29 17:57:16 +0200

    Add MedicationReceiptList class

--- services/medication/medication.py
+++ services/medication/medication.py
@@ -1,5 +1,10 @@
 from abc import ABC, abstractmethod
-from services.time_logic import time_validator_format_yyyy_mm_dd
+from services.validation_user_input.time_validator import (
+    time_validator_format_yyyy_mm_dd,
+    time_in_period,
+)
+from datetime import date
+from services.health_analysis import MedicationAnalyzer
 
 
 # frequency -> in specific days | every day | arbitrary time
@@ -64,7 +69,7 @@ class MedicationReceipt:
         ) = None
 
     def create_medication_that_adjust_for_receipt(
-        self, name, form, unit_of_measurements
+        self, name: str, form: str, unit_of_measurements: str
     ):
         self.medication_object_that_adjust = Medication(
             name, form, unit_of_measurements
@@ -108,7 +113,7 @@ class ReceiptBuilderInterface(ABC):
     """This is a Interface class for all receipt Builders"""
 
     @abstractmethod
-    def set_medication_object(self, name, form, unit_of_measurements):
+    def set_medication_object(self, name: str, form: str, unit_of_measurements: str):
         pass
 
     @abstractmethod
@@ -171,17 +176,148 @@ class ReceiptBuilder(ReceiptBuilderInterface):
 
 class MedicationReceiptList:
     """This class responsible for storage all receipts.
-    Sometimes a receipts deleted from this list if user complete a plan"""
+    Sometimes a receipts deleted from this list if user complete a plan
+    List of receipts have the next format [{Medication_obj: MedicationObjectReceiptCharacteristic}, ..., ]
+    """
+
+    __instance = None
+
+    def __new__(cls):
+        if cls.__instance is None:
+            cls.__instance = super().__new__(cls)
+        return cls.__instance
 
     def __init__(self):
-        self.receipts = []
+        if not hasattr(self, "initialize"):
+            self.receipts: list[MedicationReceipt] = []
+            self.initialize = True
 
-    def add_receipt(self, receipt: MedicationReceipt):
-        self.receipts.append(receipt)
+    def add_receipt(self, _receipt: MedicationReceipt):
+        self.receipts.append(_receipt)
 
     def delete_receipt(self):
         pass
 
+    def get_list_of_all_available_receipts(
+        self,
+    ) -> list[MedicationReceipt]:
+        return self.receipts
+
+
+def convert_list_of_medication_to_dict_with_status(
+    list_of_medications: list[Medication],
+) -> dict[Medication, bool]:
+    """This functions convert list of Medication objects to dict with status. Status == False means that user don't
+    take medication. Status == True means that user take medication. On start of day
+    all medication objects has STATUS == False"""
+    return {med_object: False for med_object in list_of_medications}
+
+
+class MedicationManager:
+    def __init__(self, list_of_receipts: MedicationReceiptList):
+        self.list_of_receipts: MedicationReceiptList = list_of_receipts
+        self.medication_analyzer: MedicationAnalyzer | None = None
+
+    def add_medication_receipt(self, receipt: MedicationReceipt):
+        self.list_of_receipts.add_receipt(receipt)
+
+    def get_list_of_all_available_receipts(
+        self,
+    ) -> list[MedicationReceipt]:
+        return self.list_of_receipts.get_list_of_all_available_receipts()
+
+    def get_medications_that_need_to_take_today(self) -> dict[Medication, bool]:
+        """This method filter all meds objects (Medication class)
+        from list_of_receipts object if data of curr day enter in interval of take this medication
+        For example [{med_obj : characteristic}] if curr date in interval of characteristic (start_date: end_date)
+        and frequency (for example if frequency = list of days, we need to check the current day name) is fitting
+        """
+        today_date = str(date.today())
+        date_obj = date.fromisoformat(today_date)
+        number_of_day = date_obj.weekday()
+        today_day_name = dict_for_days[number_of_day]
+        lst_of_med_objs_that_need_to_take_today = []
+        for (
+            medication_receipt_obj
+        ) in self.list_of_receipts.get_list_of_all_available_receipts():
+            med_obj = list(
+                medication_receipt_obj.dict_of_medications_in_receipt.keys()
+            )[0]
+            if (
+                receipt.dict_of_medications_in_receipt[med_obj].frequency == "everyday"
+                and receipt.dict_of_medications_in_receipt[med_obj].interval == "always"
+                or time_in_period(
+                    receipt.dict_of_medications_in_receipt[
+                        med_obj
+                    ].start_time_of_interval,
+                    receipt.dict_of_medications_in_receipt[
+                        med_obj
+                    ].end_time_of_interval,
+                    today_date,
+                )
+            ):
+                lst_of_med_objs_that_need_to_take_today.append(med_obj)
+            elif (
+                len(receipt.dict_of_medications_in_receipt[med_obj].list_of_days) != 0
+                and receipt.dict_of_medications_in_receipt[med_obj].interval == "always"
+                or time_in_period(
+                    receipt.dict_of_medications_in_receipt[
+                        med_obj
+                    ].start_time_of_interval,
+                    receipt.dict_of_medications_in_receipt[
+                        med_obj
+                    ].end_time_of_interval,
+                    today_date,
+                )
+            ):
+                if (
+                    today_day_name
+                    in receipt.dict_of_medications_in_receipt[med_obj].list_of_days
+                ):
+                    lst_of_med_objs_that_need_to_take_today.append(med_obj)
+            elif (
+                receipt.dict_of_medications_in_receipt[med_obj].frequency == "arbitrary"
+            ):
+                lst_of_med_objs_that_need_to_take_today.append(med_obj)
+        return convert_list_of_medication_to_dict_with_status(
+            lst_of_med_objs_that_need_to_take_today
+        )
+
+    def took_medication_object(self, medication_object: Medication):
+        ...
+        receipt_obj = self.find_receipt_with_appropriate_med_obj(medication_object)
+        ...
+        if self.receipt_is_completed(receipt_obj):
+            self.delete_receipt(receipt_obj)
+
+    def receipt_is_completed(self, receipt_obj: MedicationReceipt) -> bool:
+        return self.medication_analyzer.receipt_is_completed(receipt_obj)
+
+    def delete_receipt(self, _receipt_obj: MedicationReceipt):
+        lst = self.list_of_receipts.get_list_of_all_available_receipts()
+        for receipt_obj in lst:
+            if receipt_obj == _receipt_obj:
+                lst.remove(receipt_obj)
+
+    def find_receipt_with_appropriate_med_obj(
+        self, medication_obj: Medication
+    ) -> MedicationReceipt | None:
+        # receipt_list = [{med_obj:characteristic},...,{}]
+        for _receipt in self.list_of_receipts.get_list_of_all_available_receipts():
+            if medication_obj in _receipt:
+                return _receipt
+        return None
+
+
+dict_for_days = {
+    1: "Monday",
+    2: "Tuesday",
+    3: "Wednesday",
+    4: "Thursday",
+    5: "Friday",
+    6: "Saturday",
+    7: "Sunday",
+}
 
 if __name__ == '__main__':
     receipt_builder = ReceiptBuilder()
