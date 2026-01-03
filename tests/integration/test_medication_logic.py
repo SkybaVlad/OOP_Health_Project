7a72eb24b0ca5148c185b0551e3a900f079e1d5b
vladskyba <skyba.vlad@lll.kpi.ua>
2026-01-03 23:30:48 +0200

    Add tests to test new functionality in medication manager class

--- tests/integration/test_medication_logic.py
+++ tests/integration/test_medication_logic.py
@@ -1,6 +1,9 @@
 from unittest.mock import Mock, patch
 import unittest
 import datetime
+from sys import path
+
+path.append('C:/Users/user/PycharmProjects/OOP_Health_Project')
 from services.medication.medication_objects import (
     MedicationReceipt,
     Medication,
@@ -10,44 +13,36 @@ from services.medication.medication_objects import (
 from facade_logic.facade_dairy_manager import DairyFacade
 from services.medication.medication_manager import MedicationManager
 from services.health_analysis import MedicationAnalyzer
-from services.health_daily.daily_health import HealthDaily
 from data.health_diary_container import HealthDiary
-from datetime import date
+from datetime import date as real_date
+from services.health_daily.daily_health import HealthDaily
 
 
-# create receipt with 2 medication objs
-# create receipt with 1 medication objs
-
-# get list of medication that need to take today
-
-# take some medication -> view if medication added to list in day obj
-# complete plan and check if meds objs deleted and receipts is deleted
+medication_manager: MedicationManager | None = None
 
 
-class TestMedicationManager(unittest.TestCase):
+def setUpModule():
+    global medication_manager
+    first_day = HealthDaily(str(datetime.date.today()))
+    health_diary = HealthDiary()
+    list_of_receipts = MedicationReceiptList()
+    medication_analyzer = MedicationAnalyzer(health_diary, list_of_receipts)
+    medication_manager = MedicationManager(list_of_receipts, health_diary)
+    medication_manager.set_medication_analyzer(medication_analyzer)
+    facade_dairy = DairyFacade(health_diary, first_day, medication_manager)
+
+
+class TestMedicationFunctionality(unittest.TestCase):
     @classmethod
     def setUpClass(cls):
-        first_day = HealthDaily(str(datetime.date.today()))
-        health_diary = HealthDiary()
-
-        list_of_receipts = MedicationReceiptList()
-
-        medication_analyzer = MedicationAnalyzer(health_diary, list_of_receipts)
-
-        cls.medication_manager = MedicationManager(list_of_receipts)
-        cls.medication_manager.set_medication_analyzer(medication_analyzer)
-
-        cls.facade_dairy = DairyFacade(health_diary, first_day, cls.medication_manager)
-
         builder = MedicationCharacteristicBuilder()
-
         cls.med_obj_1_rec_1 = Medication("Aspirin", "Capsule", "ml")
 
         cls.chars_1_rec_1 = (
             builder.set_medication_dosage_per_one_take(200)
             .set_frequency("Every day")
             .set_interval("Choose specific interval")
-            .set_start_time(str(datetime.date.today()))
+            .set_start_time("2026-01-01")
             .set_end_time("2026-01-07")
             .get_result()
         )
@@ -56,7 +51,7 @@ class TestMedicationManager(unittest.TestCase):
         cls.chars_2_rec_1 = (
             builder.reset()
             .set_medication_dosage_per_one_take(50)
-            .set_frequency("Specific days")
+            .set_frequency("Choose specific days")
             .set_list_of_days(["Monday", "Sunday"])
             .set_interval("Forever")
             .get_result()
@@ -82,10 +77,10 @@ class TestMedicationManager(unittest.TestCase):
         cls.chars_4_rec_3 = (
             builder.reset()
             .set_medication_dosage_per_one_take(10)
-            .set_frequency("Specific days")
+            .set_frequency("Choose specific days")
             .set_list_of_days(["Tuesday", "Friday"])
             .set_interval("Choose specific interval")
-            .set_start_time(str(datetime.date.today()))
+            .set_start_time("2026-01-01")
             .set_end_time("2026-01-04")
             .get_result()
         )
@@ -93,11 +88,12 @@ class TestMedicationManager(unittest.TestCase):
         cls.receipt_3 = MedicationReceipt()
         cls.receipt_3.add_pair_to_receipt(cls.med_obj_4_rec_3, cls.chars_4_rec_3)
 
-        cls.medication_manager.add_medication_receipt(cls.receipt_1)
-        cls.medication_manager.add_medication_receipt(cls.receipt_2)
-        cls.medication_manager.add_medication_receipt(cls.receipt_3)
+        medication_manager.add_medication_receipt(cls.receipt_1)
+        medication_manager.add_medication_receipt(cls.receipt_2)
+        medication_manager.add_medication_receipt(cls.receipt_3)
 
     def test_creation_functionality(self):
+        # test whether receipt objects created correctly
         self.assertEqual(self.chars_1_rec_1.medication_dosage_per_one_take, 200)
         self.assertEqual(self.chars_1_rec_1.frequency, "Every day")
         self.assertEqual(self.chars_1_rec_1.list_of_days, None)
@@ -105,11 +101,11 @@ class TestMedicationManager(unittest.TestCase):
 
     def test_receipt_add_functionality(self):
 
-        self.assertEqual(len(self.medication_manager.list_of_receipts.receipts), 3)
+        self.assertEqual(len(medication_manager.list_of_receipts.receipts), 3)
 
-        self.assertIn(self.receipt_1, self.medication_manager.list_of_receipts.receipts)
-        self.assertIn(self.receipt_2, self.medication_manager.list_of_receipts.receipts)
-        self.assertIn(self.receipt_3, self.medication_manager.list_of_receipts.receipts)
+        self.assertIn(self.receipt_1, medication_manager.list_of_receipts.receipts)
+        self.assertIn(self.receipt_2, medication_manager.list_of_receipts.receipts)
+        self.assertIn(self.receipt_3, medication_manager.list_of_receipts.receipts)
 
         self.assertTrue(self.receipt_1.is_exist_med_obj(self.med_obj_1_rec_1))
         self.assertTrue(self.receipt_1.is_exist_med_obj(self.med_obj_2_rec_1))
@@ -134,11 +130,10 @@ class TestMedicationManager(unittest.TestCase):
         )
 
     def test_get_list_of_med_objs_that_need_to_take_today_functionality(self):
-
-        with patch("services.medication.medication_manager.date") as mock_date:
-            mock_date.today.return_value = date(2026, 1, 2)
+        with patch("services.health_analysis.date") as mock_date:
+            mock_date.today.return_value = real_date(2026, 1, 2)
             self.assertEqual(
-                self.medication_manager.get_list_of_medications_that_need_to_take_today(),
+                medication_manager.get_list_of_medications_that_need_to_take_today(),
                 [self.med_obj_1_rec_1, self.med_obj_3_rec_2, self.med_obj_4_rec_3],
             )
 
@@ -147,14 +142,38 @@ class TestMedicationManager(unittest.TestCase):
         return_value=True,
     )
     def test_took_medication(self, mocked_method):
-        self.medication_manager.took_medication_object(self.med_obj_3_rec_2)
+        medication_manager.took_medication_object(self.med_obj_3_rec_2)
 
         self.assertEqual(len(self.receipt_2.dict_of_medications_in_receipt), 0)
-        self.assertEqual(len(self.medication_manager.list_of_receipts.receipts), 2)
+        self.assertEqual(len(medication_manager.list_of_receipts.receipts), 2)
 
-        self.medication_manager.took_medication_object(self.med_obj_1_rec_1)
+        medication_manager.took_medication_object(self.med_obj_1_rec_1)
 
         self.assertEqual(len(self.receipt_1.dict_of_medications_in_receipt), 1)
         self.assertNotIn(
             self.med_obj_1_rec_1, self.receipt_1.dict_of_medications_in_receipt
         )
+
+    def test_no_took_medication(self):
+        day_2: HealthDaily = HealthDaily("2026-01-02")
+        day_3: HealthDaily = HealthDaily("2026-01-03")
+        day_4: HealthDaily = HealthDaily("2026-01-04")
+        day_5: HealthDaily = HealthDaily("2026-01-05")
+        # create and add day object to list of object for test
+        # whether no_took_medication working correctly
+        medication_manager.health_diary.add_day(day_2)
+        medication_manager.health_diary.add_day(day_3)
+        medication_manager.health_diary.add_day(day_4)
+        medication_manager.health_diary.add_day(day_5)
+
+        lst = medication_manager.get_list_of_all_medication_that_user_not_take()
+        self.assertEqual(len(lst), 11)
+
+        day_5.list_of_taken_medication.append(self.med_obj_1_rec_1)
+
+        lst = medication_manager.get_list_of_all_medication_that_user_not_take()
+        self.assertEqual(len(lst), 10)
+
+        day_2.list_of_taken_medication.append(self.med_obj_1_rec_1)
+        lst = medication_manager.get_list_of_all_medication_that_user_not_take()
+        self.assertEqual(len(lst), 9)
