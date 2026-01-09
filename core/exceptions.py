0027c425588901a448830a536353549ee442a30e
vladskyba <skyba.vlad@lll.kpi.ua>
2026-01-09 14:56:17 +0200

    Add exceptions module in which implement custom exceptions

---
+++ core/exceptions.py
@@ -0,0 +1,7 @@
+class LimitCallsError(Exception):
+    """This exception raised when limit of calling something is exhausted"""
+
+    pass
+
+class DateOfDayIsGreaterThanTodayError(Exception):
+    pass
