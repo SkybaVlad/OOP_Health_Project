0c639599417f0d7d2fa7d8eeb6be9673e24591c1
vladskyba <skyba.vlad@lll.kpi.ua>
2026-01-09 15:03:39 +0200

    Resolve file problem

--- core/body_metrics_calculator.py
+++ core/body_metrics_calculator.py
@@ -13,11 +13,11 @@ class BodyMetricsType(Enum):
     percentage_of_water_level = 'fwl'
 
 
-def calculate_body_mass_index_metrics(
-    weight: float | int, height: float | int
-) -> float:
-    if type(weight) != float and type(height) != float:
-        raise TypeError('weight or height must be float or int')
+def calculate_body_mass_index_metrics(weight: float, height: float) -> float:
+    if type(weight) != float:
+        raise TypeError('weight must be float')
+    if type(height) != float:
+        raise TypeError('height must be float')
     if weight < 0:
         raise ValueError('weight must be positive')
     if height < 0:
@@ -26,17 +26,19 @@ def calculate_body_mass_index_metrics(
 
 
 def calculate_basal_metabolic_rate(
-    user_sex: str, weight: float | int, height: float | int, age: int
+    user_sex: str, weight: float, height: float, age: int
 ) -> float:
-    if type(weight) != float and type(height) != float:
-        raise TypeError('weight or height must be float or int')
+    if type(weight) != float:
+        raise TypeError('weight must be float')
+    if type(height) != float:
+        raise TypeError('height must be float')
     if weight < 0:
         raise ValueError('weight must be positive')
     if height < 0:
         raise ValueError('height must be positive')
     if type(user_sex) != str:
         raise ValueError("user sex must be str")
-    if user_sex != 'Male' or user_sex != 'Female':
+    if user_sex != 'Male' and user_sex != 'Female':
         raise ValueError("user sex should be 'Male' or 'Female'")
     if type(age) != int:
         raise ValueError("age must be int")
@@ -48,27 +50,25 @@ def calculate_basal_metabolic_rate(
         return 10 * weight + 6.25 * height - 5 * age - 161
 
 
-def calculate_lean_body_mass(weight: float | int, fat_percentage: float | int) -> float:
-    if type(weight) != float and type(weight) != int:
-        raise TypeError("weight must be float or int")
+def calculate_lean_body_mass(weight: float, fat_percentage: float) -> float:
+    if type(weight) != float:
+        raise TypeError("weight must be float")
+    if type(fat_percentage) != float:
+        raise TypeError("fat percentage must be float")
     if weight < 0:
         raise ValueError('weight must be positive')
-    if type(fat_percentage) != float and type(fat_percentage) != int:
-        raise TypeError("fat_percentage must be float or int")
     if fat_percentage < 0:
         raise ValueError("fat percentage must be positive")
     return weight * (1 - fat_percentage / 100)
 
 
-def calculate_fat_mass(
-    weight: float | int, lean_body_mass_value: float | int
-) -> float | int:
-    if type(weight) != float and type(weight) != int:
-        raise TypeError("weight must be float or int")
+def calculate_fat_mass(weight: float, lean_body_mass_value: float) -> float | int:
+    if type(weight) != float:
+        raise TypeError("weight must be float")
+    if type(lean_body_mass_value) != float:
+        raise TypeError("lean_body_mass_value must be float")
     if weight < 0:
         raise ValueError('weight must be positive')
-    if type(lean_body_mass_value) != float and type(lean_body_mass_value) != int:
-        raise TypeError("lean_body_mass_value must be float or int")
     if lean_body_mass_value < 0:
         raise ValueError("lean_body_mass_value must be positive")
     return weight - lean_body_mass_value
