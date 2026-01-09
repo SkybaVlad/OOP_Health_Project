36380a462c4e7dc217b76bd1ad4831023749cbb3
vladskyba <skyba.vlad@lll.kpi.ua>
2026-01-09 15:04:44 +0200

    Adding init files to packages

---
+++ core/facade_logic/__init__.py
@@ -0,0 +1,5 @@
+from sys import path
+
+path.append("C:/Users/user/PycharmProjects/OOP_Health_Project/core/facade_logic")
+
+import facade_api
---
+++ core/validation_user_input/__init__.py
@@ -0,0 +1,3 @@
+"""This package responsible for validation any user input data
+In this package we have some module where in each module describes concrete validation function
+"""
