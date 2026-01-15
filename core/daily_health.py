320090ba8e5b902009c5bc719e8ad7bd29ed47cd
vladskyba <skyba.vlad@lll.kpi.ua>
2026-01-15 22:29:12 +0200

    add setters for goals

--- core/daily_health.py
+++ core/daily_health.py
@@ -7,6 +7,10 @@ import datetime
 
 class HealthDaily:
     def __init__(self, date_of_day: str):
+        """This class contain all characteristic about day.
+
+
+        Goals value set as default value but user can define own goals"""
         self.date_of_day: str = date_of_day
         self.burned_calories_for_day: float = 0.0
         self.consumed_calories_for_day: float = 0.0
@@ -21,6 +25,10 @@ class HealthDaily:
         self.fat_percentage: float = 0.0
         self.total_time_spend_on_activities: float = 0.0
         self.name_of_day: str = self.generate_name_of_day()
+        self.water_goal_on_day: float = 0.0
+        self.burned_calories_goal_on_day: float = 0.0
+        self.consumed_calories_goal_on_day: float = 0.0
+        self.step_goal_on_day: float = 0.0
 
     def add_activity(self, activity_object: SpecificActivityType) -> None:
         self.list_of_activities_for_day.append(activity_object)
@@ -57,6 +65,22 @@ class HealthDaily:
 
     def add_burned_calories(self, burned_calories: float) -> None:
         self.burned_calories_for_day += burned_calories
+
+    def set_water_goal_on_day(self, water_goal_on_day: float) -> None:
+        self.water_goal_on_day = water_goal_on_day
+
+    def set_burned_calories_goal_on_day(
+        self, burned_calories_goal_for_day: float
+    ) -> None:
+        self.burned_calories_goal_on_day = burned_calories_goal_for_day
+
+    def set_consumed_calories_goal_on_day(
+        self, consumed_calories_goal_for_day: float
+    ) -> None:
+        self.consumed_calories_goal_on_day = consumed_calories_goal_for_day
+
+    def set_step_goal_on_day(self, step_goal_on_day: float) -> None:
+        self.step_goal_on_day = step_goal_on_day
 
     def generate_name_of_day(self) -> str:
         lst = self.date_of_day.split("-")
